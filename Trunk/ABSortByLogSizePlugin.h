b7307b150c946ef8e0c67cf35c627f7079bab82b
Jon Chambers <jon.chambers@gmail.com>
2008-09-28 18:44:36 +0000

    Initial commit of skeletal project.

---
+++ Trunk/ABSortByLogSizePlugin.h
@@ -0,0 +1,16 @@
+//
+//  ABSortByLogSizePlugin.h
+//  SortByLogSizePlugin
+//
+//  Created by Jon Chambers on 9/28/08.
+//  Copyright 2008 Jon Chamber. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+#import <Adium/AIPlugin.h>
+
+@interface ABSortByLogSizePlugin : AIPlugin
+{
+}
+
+@end
---
+++ Trunk/ABSortByLogSizePlugin.m
@@ -0,0 +1,49 @@
+//
+//  ABSortByLogSizePlugin.m
+//  SortByLogSizePlugin
+//
+//  Created by Jon Chambers on 9/28/08.
+//  Copyright 2008 Jon Chambers. All rights reserved.
+//
+
+// Important note:
+// http://adiumx.com/pipermail/adium-devl_adiumx.com/2008-September/005692.html
+
+#import "ABSortByLogSizePlugin.h"
+
+#import <Adium/AISharedAdium.h>
+#import <Adium/ESDebugAILog.h>
+
+@implementation ABSortByLogSizePlugin
+
+- (void)installPlugin
+{
+	AILog(@"ABSortByLogSizePlugin installed.");
+}
+
+- (void)uninstallPlugin 
+{
+	AILog(@"ABSortByLogSizePlugin uninstalled.");
+}
+
+- (NSString *)pluginAuthor
+{
+	return @"Jon Chambers";
+}
+
+- (NSString *)pluginVersion
+{
+	return @"development";
+}
+
+- (NSString *)pluginDescription
+{
+	return @"Allows the contact to be sorted by transcript file size.";
+}
+
+- (NSString *)pluginURL
+{
+	return @"http://projects.eatthepath.com/sort-by-log-size-plugin/";
+}
+
+@end
